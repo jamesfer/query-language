0a97a46d172e88b3eb9fa7e71481c07aa598ad0a
James Ferguson <jamesferguson497@gmail.com>
2017-10-09 18:21:25 +1100

    Improve build script.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,43 @@
 const webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './index.ts',
-  target: 'web',
-  output: {
-    publicPath: 'build',
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    library: 'qlang',
-  },
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: {
-        loader: 'awesome-typescript-loader',
-        options: {
-          silent: true,
-          sourceMap: true,
+module.exports = env => {
+  let production = env && env.production;
+  return {
+    context: path.resolve(__dirname, 'src'),
+    devtool: 'source',
+    entry: './index.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'qlang.js',
+      library: 'qlang',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        use: {
+          loader: 'awesome-typescript-loader',
+          options: {
+            silent: true,
+            sourceMap: true,
+          },
         },
-      },
-      exclude: [ /node_modules/ ],
-    }],
-  },
-  resolve: {
-    modules: [
-      'node_modules',
-    ],
-    extensions: ['.ts', '.js', '.json', '*'],
-  },
-  devtool: 'source',
+        exclude: [ /node_modules/ ],
+      }],
+    },
+    resolve: {
+      modules: [
+        'node_modules',
+      ],
+      extensions: ['.ts', '.js', '.json', '*'],
+    },
+    plugins: []
+      .concat(production ? [
+        new UglifyJSPlugin({
+          sourceMap: true,
+        }),
+      ]: []),
+  }
 };
