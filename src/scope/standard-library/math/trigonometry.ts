d4be542aa29d047ec492c9cd8e7f2c768ecadc62
James Ferguson <jamesferguson497@gmail.com>
2017-11-04 17:30:34 +1100

    Correct the type of trigonometric functions.

--- src/scope/standard-library/math/trigonometry.ts
+++ src/scope/standard-library/math/trigonometry.ts
@@ -4,32 +4,32 @@ import { bindFloatFunction } from '../../library-utils';
 
 
 const tan: LibraryEntry = {
-  type: makeFunctionType([FloatType, FloatType], FloatType),
+  type: makeFunctionType([FloatType], FloatType),
   impl: bindFloatFunction(Math.tan),
 };
 
 const sin: LibraryEntry = {
-  type: makeFunctionType([FloatType, FloatType], FloatType),
+  type: makeFunctionType([FloatType], FloatType),
   impl: bindFloatFunction(Math.sin),
 };
 
 const cos: LibraryEntry = {
-  type: makeFunctionType([FloatType, FloatType], FloatType),
+  type: makeFunctionType([FloatType], FloatType),
   impl: bindFloatFunction(Math.cos),
 };
 
 const atan: LibraryEntry = {
-  type: makeFunctionType([FloatType, FloatType], FloatType),
+  type: makeFunctionType([FloatType], FloatType),
   impl: bindFloatFunction(Math.atan),
 };
 
 const asin: LibraryEntry = {
-  type: makeFunctionType([FloatType, FloatType], FloatType),
+  type: makeFunctionType([FloatType], FloatType),
   impl: bindFloatFunction(Math.asin),
 };
 
 const acos: LibraryEntry = {
-  type: makeFunctionType([FloatType, FloatType], FloatType),
+  type: makeFunctionType([FloatType], FloatType),
   impl: bindFloatFunction(Math.acos),
 };
 
