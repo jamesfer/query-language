120ae7e4bfc9946dd06d9b21d52a5213a9a4f9ca
James Ferguson <jamesferguson497@gmail.com>
2017-11-06 19:31:15 +1100

    Remove old commented implementation.

--- src/scope/standard-library/array/in.ts
+++ src/scope/standard-library/array/in.ts
@@ -15,56 +15,6 @@ import {
 import { LibraryEntry } from '../../library';
 import { evaluateArguments } from '../../library-utils';
 
-// function inFunc(item: Value, list: ArrayValue): PromiseValue<BooleanValue> {
-//   // return Promise.all([a(), b()]).then(([item, list]) => {
-//     let iterator = list.value/* as Iterator<Promise<Value>>*/;
-//     let { value: element, done } = iterator.next();
-//
-//     return new Promise<BooleanValue>(resolve => {
-//       if (done) {
-//         resolve(TrueValue);
-//       }
-//       else {
-//         let promise: Promise<Value | undefined>;
-//         let checkValue = (el: Value | undefined) => {
-//           if (!el) {
-//             resolve(FalseValue);
-//             return;
-//           }
-//
-//           if (el.value === item.value) {
-//             resolve(TrueValue);
-//             return;
-//           }
-//
-//           let next = iterator.next();
-//           if (next.done) {
-//             resolve(FalseValue);
-//             return;
-//           }
-//
-//           promise = promise.then<Value | undefined>(checkValue);
-//           return next.value;
-//         };
-//         promise = Promise.resolve(element).then<Value | undefined>(checkValue);
-//       }
-//     });
-//   // });
-// }
-
-// function inFunc(a: Value, b: ArrayValue): BooleanValue {
-//   let aValue = a.value;
-//   let iterator = b.value;
-//   let { value: element, done } = iterator.next();
-//
-//   while (!done) {
-//     if (element.value === aValue) {
-//       return BooleanTrueValue;
-//     }
-//     ({ value: element, done } = iterator.next());
-//   }
-//   return BooleanFalseValue;
-// }
 
 function inFunc(item: Value, list: ArrayValue): LazyValue<BooleanValue> {
   return list.value.first(el => el.value === item.value, () => true, false)
