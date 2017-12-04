3750bb62030bae3af47897ed92d611f377034e0e
James Ferguson <jamesferguson497@gmail.com>
2017-12-05 01:33:01 +1100

    Simplify string literal evaluator.

--- src/evaluate/evaluators/string-literal.ts
+++ src/evaluate/evaluators/string-literal.ts
@@ -4,7 +4,5 @@ import { LazyValue, makeStringValue, StringValue } from '../../value.model';
 import { EvaluationScope } from '../evaluation-scope';
 
 export function evaluateStringLiteral(scope: EvaluationScope, expression: StringLiteralExpression): LazyValue<StringValue> {
-  let value = expression.tokens[0].value;
-  value = value.slice(1, value.length - 1);
-  return Observable.of(makeStringValue(value));
+  return Observable.of(makeStringValue(expression.value));
 }
