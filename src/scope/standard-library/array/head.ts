f1f042156e48af61ca1d714ba035eb76212f156d
James Ferguson <jamesferguson497@gmail.com>
2017-12-25 22:03:42 +1100

    Add missing rxjs import.

--- src/scope/standard-library/array/head.ts
+++ src/scope/standard-library/array/head.ts
@@ -5,6 +5,7 @@ import {
 } from '../../../type.model';
 import { ArrayValue, LazyValue, } from '../../../value.model';
 import { LibraryEntry } from '../../library';
+import 'rxjs/add/operator/switchMap';
 
 function headFunc(list: LazyValue<ArrayValue>): LazyValue {
   return list.switchMap(arrValue => arrValue.value.first());
