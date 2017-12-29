eaf611e35b84d71efb17a64a33ac4f599be20857
James Ferguson <jamesferguson497@gmail.com>
2017-12-30 02:05:34 +1100

    Correctly export comparator functions through the math module.

--- src/standard-library/standard-library.ts
+++ src/standard-library/standard-library.ts
@@ -2,13 +2,11 @@ import { Library } from './library';
 import { _if } from './branching/if';
 import { math } from './math/math';
 import { otherOperators } from './other-operators';
-import { comparators } from './math/comparators';
 import { array } from './array/array';
 
 export const standardLibrary: Library = {
   ...math,
   ...otherOperators,
-  ...comparators,
   ...array,
 
   'if': _if,
