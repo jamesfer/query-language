d809abca73840e5586d151d00017560ff8665505
James Ferguson <jamesferguson497@gmail.com>
2018-03-27 08:17:05 +1100

    Deploy the demo to gh-pages branch after compiling.

--- scripts/compile-demo.sh
+++ scripts/compile-demo.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Abort if any command fails
+set -o errexit
+
 echo "Compiling the editor"
 cd packages/editor
 yarn build
@@ -8,6 +11,13 @@ echo "Compiling the demo"
 cd ../demo
 yarn prod
 
-#echo "Deploying the demo"
-#cp -R dist /tmp/demo-dist/
-
+echo "Deploying the demo"
+git worktree add gh-pages gh-pages
+rm -r gh-pages/*
+cp -r dist/* gh-pages
+cd gh-pages
+git add .
+git commit -m "CHORE: Update demo."
+git push
+cd ../
+rm -r gh-pages
