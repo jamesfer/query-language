bcadd0a4fab0224c1e1dac2037f3e15cd8995d02
James Ferguson <jamesferguson497@gmail.com>
2018-03-28 22:13:18 +1100

    Add more typing tests.

--- packages/core/tests/cases/types.test.ts
+++ packages/core/tests/cases/types.test.ts
@@ -1,10 +1,18 @@
 import {
-  addToken, functionCallExpression, identifierExpression, integerExpression, integerToken,
+  addToken, booleanExpression, booleanToken, closeParenToken, commaToken, floatExpression,
+  floatToken,
+  functionCallExpression,
+  identifierExpression,
+  identifierToken,
+  integerExpression,
+  integerToken, openParenToken,
   stringExpression,
   stringToken,
 } from '../utils';
 import { evaluates } from '../runner';
-import { floatType, makeFunctionType } from '../../src/type/constructors';
+import {
+  booleanType, floatType, makeFunctionType, stringType,
+} from '../../src/type/constructors';
 import { makeMessage } from '../../src/message';
 
 let addIdentifier = identifierExpression(
@@ -12,7 +20,42 @@ let addIdentifier = identifierExpression(
   '+',
 );
 
-describe.only('type checking', () => {
+describe('type checking', () => {
+  evaluates('calculates partial function type', '1.5 +', {
+    compiled: true,
+    evaluated: true,
+    tokens: [
+      floatToken('1.5'),
+      addToken(1),
+    ],
+    expression: functionCallExpression(
+      makeFunctionType([ floatType ], floatType),
+      addIdentifier,
+      [ floatExpression(1.5) ],
+    ),
+  });
+
+  evaluates('calculates polymorphic partial function types', 'if(true, "Is true")', {
+    compiled: true,
+    evaluated: true,
+    tokens: [
+      identifierToken('if'),
+      openParenToken(),
+      booleanToken('true'),
+      commaToken(),
+      stringToken('"Is true"', 1),
+      closeParenToken(),
+    ],
+    expression: functionCallExpression(
+      makeFunctionType([ stringType ], stringType),
+      identifierExpression(makeFunctionType([ booleanType, 'T', 'T' ], 'T'), 'if'),
+      [
+        booleanExpression(true),
+        stringExpression("Is true"),
+      ]
+    )
+  });
+
   evaluates('emits type error', '1 + "1"', {
     compiled: false,
     evaluated: false,
